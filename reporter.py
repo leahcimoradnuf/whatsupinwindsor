01a7c97b8c24a93498966453fdc08a7a47b18f27
Mike Fundaro <mikefundaro@gmail.com>
2026-02-20 09:42:43 -0500

    cloned repo locally for development here

--- reporter.py
+++ reporter.py
@@ -1,1 +1,17 @@
-# Module to create Zoom recording bot
+# Module to create reporting bot
+
+def attend_zoom():
+    """
+    function for interfacing with 3rd party zoom recording bot
+    
+    returns text transcript    
+    """
+    pass
+
+def fetch_documents():
+    """
+    Recieve assignment and extract pdf/text of a meeting document
+
+    returns text transcript
+    """
+    pass
