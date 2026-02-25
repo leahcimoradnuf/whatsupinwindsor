ee37e8de4163399c97c059cf5c83c0183bea9ea0
Mike Fundaro <mikefundaro@gmail.com>
2026-02-25 15:25:13 -0500

    cleaned up project architecture and set up test environment

---
+++ wuiw/reporter.py
@@ -0,0 +1,17 @@
+# Module to create reporting bot
+
+def attend_zoom():
+    """
+    function for interfacing with 3rd party zoom recording bot
+    
+    returns text transcript    
+    """
+    pass
+
+def fetch_documents():
+    """
+    Recieve assignment and extract pdf/text of a meeting document
+
+    returns text transcript
+    """
+    pass
