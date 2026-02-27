862d3272a0a79301f33f0a478c88b8dcec837b94
Mike Fundaro <mikefundaro@gmail.com>
2026-02-26 20:15:57 -0500

    commit before rebase

--- wuiw/intake.py
+++ wuiw/intake.py
@@ -1,52 +1,64 @@
 # module for reading and parsing RSS feeds
 import feedparser
+from datetime import datetime
 
-#TODO there is no etag or last modified header, make this handle it with published parsed
-def get_rss(rss_url): #, etag=None, modified=None):
+#TODO get modified to pass into the function as a datetime.datetime type, not nonetype
+def get_rss(rss_url, modified=None): #, etag=None, modified=None):
     """
     Read RSS feed.
 
     Returns:
-        (new_entries_dict, etag, modified)
-        OR (None, etag, modified) if 304
+        (new_entries_dict, modified)
+        OR (None, modified) if 304
     """
+
+    def parsedate(date_string):
+        """helper script for reading last-modified date"""
+        format_string = '%a, %d %b %Y %H:%M:%S GMT'
+        last_modified = datetime.strptime(date_string, format_string)
+        return last_modified
 
     feed = feedparser.parse(rss_url)
 
     # --- Nothing new ---
     if feed.status == 304:
         print("No new RSS entries")
-        return None, etag, modified
+        return None, modified
 
     # --- New content ---
     if feed.status == 200:
-        new_entries = {}
+        last_modified = parsedate(feed['headers']['last-modified'])
+        if not modified or modified and last_modified > modified:
+            new_entries = {}
 
-        for entry in feed.entries:
-            id_parts = entry["id"].split("/")
-            id = id_parts[-2]
-            year = entry["published_parsed"][0]
-            month = entry["published_parsed"][1]
-            day = entry["published_parsed"][2]
+            for entry in feed.entries:
+                id_parts = entry["id"].split("/")
+                id = id_parts[-2]
+                year = entry["published_parsed"][0]
+                month = entry["published_parsed"][1]
+                day = entry["published_parsed"][2]
 
-            url = (
-                f"https://www.windsorct.gov/AgendaCenter/"
-                f"ViewFile/Agenda/_{month:02d}{day:02d}{year}-{id}?html=true"
-            )
+                url = (
+                    f"https://www.windsorct.gov/AgendaCenter/"
+                    f"ViewFile/Agenda/_{month:02d}{day:02d}{year}-{id}?html=true"
+                )
 
-            new_entries[id] = {
-                "year": year,
-                "month": month,
-                "day": day,
-                "hour": entry["published_parsed"][3],
-                "minute": entry["published_parsed"][4],
-                "url": url
-            }
+                new_entries[id] = {
+                    "year": year,
+                    "month": month,
+                    "day": day,
+                    "hour": entry["published_parsed"][3],
+                    "minute": entry["published_parsed"][4],
+                    "url": url
+                }
+            print(last_modified)
 
-        return new_entries
+            return new_entries, last_modified
+        # else:
+            # print("No new RSS entries")
 
     # --- Error case ---
-    print(f"No RSS feed found. Status: {feed.status}")
+    print(f"No new RSS entries found. Status: {feed.status}")
     return None
 
 def assign(url):
