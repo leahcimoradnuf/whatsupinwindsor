ac74d9b738d7982ec4dd7c176e1dc474340a6921
Mike Fundaro <mikefundaro@gmail.com>
2026-03-02 11:54:08 -0500

    small refactor, moved state variables from intake.py to config.py

--- wuiw/config.py
+++ wuiw/config.py
@@ -1,6 +1,11 @@
+# HTTP header info
 USER_AGENT = "WUIW/0.1 (+https://app.whatsupinwindsor.com; contact: mike@whatsupinwindsor.com)"
 
 HEADERS = {
     "User-Agent": USER_AGENT,
     "Accept": "application/rss+xml, application/xml;q=0.9, */*;q=0.8",
 }
+
+# State files
+STATE_FILE = "modified_state.json"
+ASSIGNMENT_LIST = "assignments.json"
