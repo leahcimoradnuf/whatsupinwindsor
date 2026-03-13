d3b49001f5a686b27b488154eeff0f062a4db234
Mike Fundaro <mikefundaro@gmail.com>
2026-03-13 15:57:38 -0400

    stubbed out flask app, structured articles json manually

--- wuiw/app.py
+++ wuiw/app.py
@@ -1,1 +1,11 @@
-#TODO create the Flask app that presents data
+from flask import Flask, render_template, abort
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    # render chronological list of all articles
+
+@app.route("/article/<meeting_id>")
+def article(meeting_id):
+    # render single article
