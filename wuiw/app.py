5e6a43e8a0b9c78c7846998e82253f5e8c6f9afe
Mike Fundaro <mikefundaro@gmail.com>
2026-03-16 07:46:48 -0400

    Flask app renders properly with json articles schema

--- wuiw/app.py
+++ wuiw/app.py
@@ -1,11 +1,34 @@
+import json
 from flask import Flask, render_template, abort
+from wuiw.config import ARTICLES_FILE
 
 app = Flask(__name__)
 
+@app.route("/home")
 @app.route("/")
 def index():
-    # render chronological list of all articles
+    with open(ARTICLES_FILE, 'r') as f:
+        articles = json.load(f)
+    sorted_articles = dict(sorted(articles.items(), key=lambda item: item[1]['meeting_date'], reverse=True))
+    return render_template("index.html", articles=sorted_articles)
 
-@app.route("/article/<meeting_id>")
+@app.route("/articles/<meeting_id>")
 def article(meeting_id):
     # render single article
+    with open(ARTICLES_FILE, 'r') as f:
+        articles = json.load(f)
+    if meeting_id not in articles:
+        abort(404)
+    return render_template("article.html", article=articles[meeting_id])
+
+@app.route("/report-error")
+def report_error():
+    return render_template("report_error.html")
+
+@app.route("/about")
+def about():
+    abort(404)
+
+@app.route("/support")
+def support():
+    abort(404)
--- wuiw/config.py
+++ wuiw/config.py
@@ -13,6 +13,7 @@ RSS_URL="https://www.windsorct.gov/RSSFeed.aspx?ModID=65&CID=All-0"
 
 # Test Data
 RSS_CURL="http://localhost:8000/town_council_rss.xml"
+ARTICLES_FILE="/home/mike/myprojects/whatsupinwindsor/test_data/articles.json"
 
 # Classification Data
 MUNICIPAL_BODIES = ["Town Council", "Planning Commission", "Board of Education"]
