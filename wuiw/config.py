7f90586edc5ea17a71d1a240706a5fb171abd5fe
Mike Fundaro <mikefundaro@gmail.com>
2026-03-17 10:56:48 -0400

    initial commit for editor

--- wuiw/config.py
+++ wuiw/config.py
@@ -1,5 +1,11 @@
 
 
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # HTTP header info
 USER_AGENT = "WUIW/0.1 (+https://app.whatsupinwindsor.com; contact: mike@whatsupinwindsor.com)"
 
@@ -10,6 +16,16 @@ HEADERS = {
 
 # Scraper Target
 RSS_URL="https://www.windsorct.gov/RSSFeed.aspx?ModID=65&CID=All-0"
+
+# Database Configuration
+def get_db_connection():
+    return psycopg2.connect(
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT")
+    )
 
 # Test Data
 RSS_CURL="http://localhost:8000/town_council_rss.xml"
