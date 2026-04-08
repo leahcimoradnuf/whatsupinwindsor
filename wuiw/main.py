145442f76868b22922eaf60ab83f9b7ca73e0fb0
Mike Fundaro <mikefundaro@gmail.com>
2026-04-08 13:43:34 -0400

    created log.py, need to unit test

--- wuiw/main.py
+++ wuiw/main.py
@@ -1,7 +1,8 @@
 import logging
+from datetime import datetime
 from wuiw.intake import get_rss
-from wuiw.config import RSS_URL
-from wuiw.editor import save_articles, save_assignments, update_status, assign
+from wuiw.config import RSS_URL, STATUS_COMPLETE, STATUS_WARNING, STATUS_FAILED
+from wuiw.editor import save_articles, save_assignments, update_status, assign, open_intake, close_intake
 from wuiw.reporter import fetch_documents
 from wuiw.writer import write_article
 
@@ -17,47 +18,86 @@ def main():
     """
     Connect to RSS feed for Town Council and summarize new meeting documents
     """
-    logger.info("WUIW pipeline started")
+    # Initialize run conditions
+    run_id = None
+    new_assignments = 0
+    failed_assignments = 0
+    status = STATUS_FAILED
+    error = None
 
-    # Fetch RSS feed
-    logger.info("Pinging RSS feed.")
     try:
-        new_leads = get_rss(RSS_URL)
-    except Exception as e:
-        logger.error("Feed parsing failed: %s", e)
-        exit(1)
-    logger.info(f"RSS fetch returned {len(new_leads)} new leads")
-    
-    # Enter new assignments to db table: assignments
-    save_assignments(new_leads)
-    logger.info(f"Assignments saved")
+        run_id = open_intake(datetime.now())
+        logger.info("WUIW pipeline started")
 
-    # Compile a list of pending assignments
-    assignments = assign()
-    logger.info(f"{len(assignments)} assignments pending")
+        # Fetch RSS feed
+        logger.info("Pinging RSS feed.")
+        try:
+            new_leads = get_rss(RSS_URL)
+        except Exception as e:
+            logger.error("Feed parsing failed: %s", e)
+            status = STATUS_FAILED
+            error = "Feed parsing failed, see logs for details"
+            raise
+        logger.info(f"RSS fetch returned {len(new_leads)} new leads")
+        
+        # Enter new assignments to db table: assignments
+        save_assignments(new_leads)
+        logger.info(f"Assignments saved")
 
-    # Send assignments to reporter to transcribe docs. 
-    # TODO: Parameter doc_type is set to "minutes" for v1.0, update in later versions
-    for assignment in assignments:
-        documents, status, error = fetch_documents(assignment["materials"], doc_type="minutes")
-        if not documents:
-            update_status(assignment["meeting_id"], status, error)
-            logger.warning(f"fetch_documents failed for {assignment['meeting_id']}: {error}")
-            continue
-        logger.info(f"Fetched {len(documents)} documents for {assignment['meeting_id']}")
+        # Compile a list of pending assignments
+        assignments = assign()
+        new_assignments = len(assignments)
+        logger.info(f"{len(assignments)} assignments pending")
 
-        # Summarize documents. 
-        for doc_type, text in documents.items():
-            article, status, error = write_article(assignment["meeting_id"], text, doc_type)
-            if not article:
+        # Send assignments to reporter to transcribe docs. 
+        # TODO: Parameter doc_type is set to "minutes" for v1.0, update in later versions
+        for assignment in assignments:
+            failed = False
+            documents, status, error = fetch_documents(assignment["materials"], doc_type="minutes")
+            if not documents:
                 update_status(assignment["meeting_id"], status, error)
-                logger.warning(f"write_article failed for {assignment['meeting_id']}: {error}")
+                logger.warning(f"fetch_documents failed for {assignment['meeting_id']}: {error}")
+                failed = True
                 continue
-            save_articles([article])
-            logger.info(f"Article saved: {article['meeting_id']} {doc_type}")
-            update_status(article["meeting_id"], status, error)
+            logger.info(f"Fetched {len(documents)} documents for {assignment['meeting_id']}")
 
-    logger.info("WUIW pipeline complete")
+            # Summarize documents. 
+            for doc_type, text in documents.items():
+                article, status, error = write_article(assignment["meeting_id"], text, doc_type)
+                if not article:
+                    update_status(assignment["meeting_id"], status, error)
+                    logger.warning(f"write_article failed for {assignment['meeting_id']}: {error}")
+                    failed = True
+                    continue
+                save_articles([article])
+                logger.info(f"Article saved: {article['meeting_id']} {doc_type}")
+                update_status(article["meeting_id"], status, error)
+            
+            # count failure
+            if failed:
+                failed_assignments += 1
+        
+        if failed_assignments == new_assignments:
+            status = STATUS_FAILED
+            error = "all assignments failed"
+        elif failed_assignments > 0 and failed_assignments < new_assignments:
+            status = STATUS_WARNING
+            error = f"{failed_assignments} assignments failed"
+        else:
+            status = STATUS_COMPLETE
+            error = None
+
+        logger.info("WUIW pipeline complete")
+    except Exception as e:
+        status = STATUS_FAILED
+        error = "WUIW pipeline failed, see logs for details"
+        logger.error("WUIW pipeline failed. Error: %s", e)
+    finally:
+        if not run_id:
+            logger.warning("WUIW pipeline not captured in intake_records. It may not have succeeded. Check logs for details.")
+        else:
+            close_intake(run_id, datetime.now(), status, new_assignments, failed_assignments, error=error)
+        
 
 if __name__ == "__main__":
     main()
